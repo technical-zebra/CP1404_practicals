9114bb317fe4fa48a5801f1ac949433e46315ff3
TZ <ke.zhang3@my.jcu.edu.au>
2021-12-28 18:58:58 +0800

    Complete ProgrammingLanguage program

---
+++ prac6_KeZhang/languages.py
@@ -0,0 +1,6 @@
+from prac6_KeZhang.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(ruby, python, visual_basic)
