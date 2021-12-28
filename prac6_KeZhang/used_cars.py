69ed8964cc040697a37a3fcffb5b83bbdac1fd11
TZ <ke.zhang3@my.jcu.edu.au>
2021-12-28 18:58:13 +0800

    Complete Car program

---
+++ prac6_KeZhang/used_cars.py
@@ -0,0 +1,34 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac6_KeZhang.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("Car",180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    # Create a new Car object called "limo" that is initialised with 100 units of fuel.
+    limo = Car("Limo", 100)
+
+    # Add 20 more units of fuel to this new car object using the add method.
+    limo.add_fuel(20)
+
+    # Print the amount of fuel in the car.
+    print(f"{limo.fuel}")
+
+    # Attempt to drive the car 115 km using the drive method.
+    limo.drive(115)
+
+    # Print the car's odometer reading.
+    print(f'{limo.odometer}')
+
+
+main()
