064fc5363824cc7b37ae436b543d99d1295ffc2a
TZ <ke.zhang3@my.jcu.edu.au>
2021-12-28 18:58:42 +0800

    Complete ProgrammingLanguage class

---
+++ prac6_KeZhang/programming_language.py
@@ -0,0 +1,23 @@
+"""CP1404/CP5632 Practical - Car class """
+
+
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Determine whether a ProgrammingLanguage is dynamic or not."""
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return f'{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}'
